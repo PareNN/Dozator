a6f9a4187ac20ba09e83c7082c37f929e929687f
PareNN <95346319+PareNN@users.noreply.github.com>
2024-04-12 14:17:54 +0300

    Update encoder.ino

--- Dozator_v1.1/encoder.ino
+++ Dozator_v1.1/encoder.ino
@@ -1,12 +1,12 @@
 void Encoder() {
 
 if (eb.left()) {
-  if (cfg.litr_m >=0.0) cfg.litr_m -= 0.2;
+  if (cfg.litr_m >=0.0) cfg.litr_m -= 0.1;
   EEPROM.put(0, cfg);
 }
 
 if (eb.right()) {
-  if (cfg.litr_m <=60.0) cfg.litr_m += 0.2;
+  if (cfg.litr_m <=60.0) cfg.litr_m += 0.1;
   EEPROM.put(0, cfg);
 }
 
