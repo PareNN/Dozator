dbe43e5b55698b458f9a48cd3797bb4b778a5795
PareNN <95346319+PareNN@users.noreply.github.com>
2024-04-12 11:31:01 +0300

    Add files via upload

---
+++ Dozator_v1.1/Data.h
@@ -0,0 +1,7 @@
+struct  {
+
+bool status;
+float litr_m;
+
+} cfg;
+
---
+++ Dozator_v1.1/encoder.ino
@@ -0,0 +1,15 @@
+void Encoder() {
+
+if (eb.left()) {
+  if (cfg.litr_m >=0.0) cfg.litr_m -= 0.2;
+  EEPROM.put(0, cfg);
+}
+
+if (eb.right()) {
+  if (cfg.litr_m <=60.0) cfg.litr_m += 0.2;
+  EEPROM.put(0, cfg);
+}
+
+ if (eb.click()) cfg.status = !cfg.status;
+EEPROM.put(0, cfg);
+}
