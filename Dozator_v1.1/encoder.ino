02cd90fe9eb8a76b6faeeed391678f1ac9988bac
PareNN <95346319+PareNN@users.noreply.github.com>
2024-04-12 14:41:50 +0300

    Add files via upload

---
+++ Dozator_v1.1/encoder.ino
@@ -0,0 +1,15 @@
+void Encoder() {
+
+if (eb.left()) {
+  if (litr_m >=0.0) litr_m -= 0.2;
+  EEPROM.put(1, litr_m);
+}
+
+if (eb.right()) {
+  if (litr_m <=60.0) litr_m += 0.2;
+  EEPROM.put(1, litr_m);
+}
+
+ if (eb.click()) status = !status;
+EEPROM.put(0, status);
+}
